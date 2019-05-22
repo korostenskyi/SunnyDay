90091d04588c13215debc6be3c5c08f1ee0601b6
Roman Korostenskyi <romankorostensky@gmail.com>
2019-05-22 11:32:24 +0300

    Add base ui

--- app/src/main/java/com/korostenskyi/sunnyday/ui/MainActivity.kt
+++ app/src/main/java/com/korostenskyi/sunnyday/ui/MainActivity.kt
@@ -1,12 +1,15 @@
-package com.korostenskyi.sunnyday
+package com.korostenskyi.sunnyday.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.korostenskyi.sunnyday.R
 
 class MainActivity : AppCompatActivity() {
 
+    private val LAYOUT = R.layout.activity_main
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(LAYOUT)
     }
 }

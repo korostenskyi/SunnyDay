60973742662b01cdbd561d0194acf3c35e91b8d3
Roman Korostenskyi <romankorostensky@gmail.com>
2019-05-23 10:47:36 +0300

    Implement Google Places API

--- app/src/main/java/com/korostenskyi/sunnyday/ui/MainActivity.kt
+++ app/src/main/java/com/korostenskyi/sunnyday/ui/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.korostenskyi.sunnyday.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
+import com.google.android.gms.common.api.Status
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
 import com.korostenskyi.sunnyday.R
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import org.koin.android.ext.android.inject
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,13 +28,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(LAYOUT)
 
-        // Fetch data for Lviv
-        choose_location_btn.setOnClickListener {
-            fetchDayInfo()
-        }
+        initPlaces()
     }
 
-    private fun fetchDayInfo() {
+    private fun fetchDayInfo(latitude: Double, longitude: Double) {
 
         viewModel.dayInfo.observe(this@MainActivity, Observer { dayInfoWrapped ->
             if (dayInfoWrapped.data != null) {
@@ -43,9 +47,32 @@ class MainActivity : AppCompatActivity() {
         })
 
         GlobalScope.launch(Dispatchers.IO) {
-            viewModel.fetchDayInfoByCoordinates(49.0, 24.0)
+            viewModel.fetchDayInfoByCoordinates(latitude, longitude)
         }
     }
 
+    private fun initPlaces() {
 
+        Places.initialize(this@MainActivity, resources.getString(R.string.places_api_key))
+
+        val autoCompleteSupportFragment = supportFragmentManager.findFragmentById(R.id.autocomplete_fragment) as AutocompleteSupportFragment
+        autoCompleteSupportFragment.setPlaceFields(listOf(Place.Field.ID, Place.Field.NAME, Place.Field.ADDRESS, Place.Field.LAT_LNG))
+
+        autoCompleteSupportFragment.setOnPlaceSelectedListener(object : PlaceSelectionListener {
+
+            override fun onPlaceSelected(place: Place) {
+
+                val coordinates = place.latLng
+
+                val latitude = coordinates!!.latitude
+                val longitude = coordinates.latitude
+
+                fetchDayInfo(latitude, longitude)
+            }
+
+            override fun onError(status: Status) {
+                Log.e("GOOGLE PLACES", "$status")
+            }
+        })
+    }
 }
